c9334586534ee612ef3fa10542ee2ae58a1de177
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2023-08-05 00:42:00 +0100

    add auth using clerk

--- ai-saas/app/(landing)/page.tsx
+++ ai-saas/app/(landing)/page.tsx
@@ -1,9 +1,28 @@
 import { Button } from '@/components/ui/button'
+import Link from 'next/link';
 
 const LandingPage = () => {
-  return (
-    <Button>LandingPage (Unprotected)</Button>
-  )
+    return (
+        <div>
+            <div>
+                LandingPage (Unprotected)
+                <Link href='/sign-in'>
+                    <Button>
+                        Login
+                    </Button>
+                </Link>
+            </div>
+            <div>
+                LandingPage (Unprotected)
+                <Link href='/sign-up'>
+                    <Button>
+                        Register
+                    </Button>
+                </Link>
+            </div>
+        </div>
+
+    )
 }
 
 export default LandingPage;
