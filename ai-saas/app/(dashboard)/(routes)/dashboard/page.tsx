03c58ec121c83ffd080533b33f1267b1d9ecfa2b
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2023-08-04 21:28:48 +0100

    basic project setup

---
+++ ai-saas/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+const DashboardPage = () => {
+  return (
+    <Button>Wassup AI</Button>
+  )
+}
+
+export default DashboardPage;
